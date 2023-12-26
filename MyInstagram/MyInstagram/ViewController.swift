bb723a400bb796084222d9ec4f04cc264860528b
Ivan Znak <ivanznak04@gmail.com>
2023-12-26 16:38:20 +0100

    Light,dark modes

--- MyInstagram/MyInstagram/ViewController.swift
+++ MyInstagram/MyInstagram/ViewController.swift
@@ -22,7 +22,28 @@ class ViewController: UIViewController {
         ])
         view.backgroundColor = .systemBackground
         self.navigationController?.setNavigationBarHidden(true, animated: false)
+        
+        registerForTraitChanges([UITraitUserInterfaceStyle.self], handler: { (self: Self, previousTraitCollection: UITraitCollection) in
+            if self.traitCollection.userInterfaceStyle == .light {
+                lightMode()
+                
+            } else {
+                darkMode()
+            }
+        })
+        
+        func lightMode(){
+            self.navBarView.buttonAddPost.tintColor = .black
+            self.navBarView.buttonLike.tintColor = .black
+            self.navBarView.buttonMessage.tintColor = .black
+            self.navBarView.label.textColor = .black
+        }
+        func darkMode(){
+            self.navBarView.buttonAddPost.tintColor = .white
+            self.navBarView.buttonLike.tintColor = .white
+            self.navBarView.buttonMessage.tintColor = .white
+            self.navBarView.label.textColor = .white
+        }
     }
-    
 }
 
