648cf49a5e14ceefa6533a612d6326cae13be97d
Ivan Znak <ivanznak04@gmail.com>
2023-12-31 14:10:43 +0100

    CellItemStruct was added

---
+++ MyInstagram/MyInstagram/StoryCellItemStruct.swift
@@ -0,0 +1,13 @@
+//
+//  StoryCellItemStruct.swift
+//  MyInstagram
+//
+//  Created by Иван Знак on 27/12/2023.
+//
+
+import UIKit
+
+struct StoryCellItem: Hashable {
+    var image: UIImage?
+    var title: String
+}
