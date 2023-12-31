5c539ebe7d7455dc8ff5baa92e1c97f1586cdc9e
Ivan Znak <ivanznak04@gmail.com>
2023-12-31 14:11:09 +0100

    Stories Interface was added

--- MyInstagram/MyInstagram/StoryCollectionViewCell.swift
+++ MyInstagram/MyInstagram/StoryCollectionViewCell.swift
@@ -8,50 +8,79 @@
 import UIKit
 
 class StoryCollectionViewCell: UICollectionViewCell {
+    static let id = "storyCollectionViewCell"
     let avatarView = UIImageView()
     let backgroundLayer = CAGradientLayer()
-    
+    let nicknameLabel = UILabel()
     override init(frame: CGRect) {
         super.init(frame: frame)
         constraints()
         avatarViewParameters()
         backgroundLayerParameters()
+        nicknameLabelParameters()
     }
-    
+    override func layoutSubviews(){
+        super.layoutSubviews()
+        if backgroundLayer.superlayer == nil {
+            layer.addSublayer(backgroundLayer)
+            layer.addSublayer(avatarView.layer)
+        }
+        avatarViewParameters()
+        backgroundLayerParameters()
+        nicknameLabelParameters()
+        avatarView.layer.cornerRadius = CGFloat(avatarView.frame.size.width / 2)
+    }
     required init?(coder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
     }
     
     func constraints(){
         addSubview(avatarView)
+        addSubview(nicknameLabel)
         avatarView.translatesAutoresizingMaskIntoConstraints = false
+        nicknameLabel.translatesAutoresizingMaskIntoConstraints = false
         NSLayoutConstraint.activate([
-            avatarView.topAnchor.constraint(equalTo: topAnchor),
-            avatarView.bottomAnchor.constraint(equalTo: bottomAnchor),
-            avatarView.leadingAnchor.constraint(equalTo: leadingAnchor),
-            avatarView.trailingAnchor.constraint(equalTo: trailingAnchor),
-            avatarView.heightAnchor.constraint(equalTo: avatarView.widthAnchor, multiplier: 1)
+            avatarView.topAnchor.constraint(equalTo: topAnchor, constant: 10),
+            avatarView.leadingAnchor.constraint(equalTo: leadingAnchor, constant: 10),
+            avatarView.trailingAnchor.constraint(equalTo: trailingAnchor, constant: -10),
+            avatarView.heightAnchor.constraint(equalTo: avatarView.widthAnchor, multiplier: 1),
+            
+            nicknameLabel.topAnchor.constraint(equalTo: avatarView.bottomAnchor, constant: 5),
+            nicknameLabel.leadingAnchor.constraint(equalTo: leadingAnchor),
+            nicknameLabel.trailingAnchor.constraint(equalTo: trailingAnchor),
+            nicknameLabel.bottomAnchor.constraint(equalTo: bottomAnchor),
+//            nicknameLabel.heightAnchor.constraint(equalToConstant: 10)
         ])
     }
     
     func avatarViewParameters(){
         avatarView.image = UIImage(resource: .avatar)
-        avatarView.layer.cornerRadius = avatarView.frame.size.width / 2
         avatarView.clipsToBounds = true
+        avatarView.contentMode = .scaleToFill
+        avatarView.layer.borderColor = UIColor.systemBackground.cgColor
+        avatarView.layer.borderWidth = 3
     }
     
     func backgroundLayerParameters(){
-        backgroundLayer.frame = avatarView.bounds
-        backgroundLayer.cornerRadius = backgroundLayer.frame.width / 2
+        backgroundLayer.frame = CGRect(x: avatarView.frame.origin.x, y: avatarView.frame.origin.y, width: avatarView.frame.width + 5, height: avatarView.frame.height + 5)
+        backgroundLayer.cornerRadius = CGFloat(avatarView.frame.width / 2)
+        
+        let centerX = avatarView.frame.origin.x + avatarView.bounds.width / 2
+        let centerY = avatarView.frame.origin.y + avatarView.bounds.height / 2
+        backgroundLayer.position = CGPoint(x: centerX, y: centerY)
         
         let redColor = UIColor(red: 1.0, green: 0.0, blue: 0.0, alpha: 1.0)
         let orangeColor = UIColor(red: 1.0, green: 0.5, blue: 0.0, alpha: 1.0)
         let yellowColor = UIColor(red: 1.0, green: 1.0, blue: 0.0, alpha: 1.0)
         let purpleColor = UIColor(red: 0.5, green: 0.0, blue: 0.5, alpha: 1.0)
 
-        backgroundLayer.colors = [redColor, orangeColor, yellowColor, purpleColor]
+        backgroundLayer.colors = [redColor.cgColor, orangeColor.cgColor, yellowColor.cgColor, purpleColor.cgColor]
         backgroundLayer.startPoint = CGPoint(x: 0, y: 0)
         backgroundLayer.endPoint = CGPoint(x: 1, y: 0)
-        layer.addSublayer(backgroundLayer)
+    }
+    func nicknameLabelParameters(){
+        nicknameLabel.textAlignment = .center
+        nicknameLabel.setContentCompressionResistancePriority(UILayoutPriority(1000), for: .vertical)
+        nicknameLabel.setContentHuggingPriority(UILayoutPriority(1000), for: .vertical)
     }
 }
--- MyInstagram/MyInstagram/ViewController.swift
+++ MyInstagram/MyInstagram/ViewController.swift
@@ -7,43 +7,108 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+enum Section: Hashable, CaseIterable {
+    case first
+}
+
+class ViewController: UIViewController, UICollectionViewDelegate {
     let navBarView = MainNavBarView()
-    
+    lazy var storyCollectionView = UICollectionView(frame: .zero, collectionViewLayout: getCompositionalLayout())
+    var collectionDataSource: UICollectionViewDiffableDataSource<Section, StoryCellItem>!
+    var titleItems: [StoryCellItem] = []
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        view.backgroundColor = .systemBackground
+        fillTitleItems()
+        
+        registerForTraitChanges([UITraitUserInterfaceStyle.self], handler: { (self: Self, previousTraitCollection: UITraitCollection) in
+            if self.traitCollection.userInterfaceStyle == .light {
+                self.lightMode()
+                
+            } else {
+                self.darkMode()
+            }
+        })
+        
+        constraints()
+        navControllerParameters()
+        storyCollectionViewParameters()
+        
+        let cellRegistration = UICollectionView.CellRegistration<StoryCollectionViewCell, StoryCellItem> {
+            cell, indexPath, itemIdentifier in
+            cell.nicknameLabel.text = itemIdentifier.title
+            
+        }
+        
+        collectionDataSource = UICollectionViewDiffableDataSource(collectionView: storyCollectionView) { (collectionView: UICollectionView, indexPath: IndexPath, itemIdentifier: StoryCellItem) -> UICollectionViewCell? in
+            let cell = collectionView.dequeueConfiguredReusableCell(using: cellRegistration, for: indexPath, item: itemIdentifier)
+                return cell
+        }
+        
+        var firstSnapshot = NSDiffableDataSourceSnapshot<Section, StoryCellItem>()
+        firstSnapshot.appendSections([.first])
+        firstSnapshot.appendItems(titleItems, toSection: .first)
+        collectionDataSource.apply(firstSnapshot, animatingDifferences: true)
+    }
+    
+    func constraints(){
         view.addSubview(navBarView)
+        view.addSubview(storyCollectionView)
         navBarView.translatesAutoresizingMaskIntoConstraints = false
+        storyCollectionView.translatesAutoresizingMaskIntoConstraints = false
         NSLayoutConstraint.activate([
             navBarView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 5),
             navBarView.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 15),
             navBarView.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -15),
-            navBarView.heightAnchor.constraint(equalToConstant: 40)
+            navBarView.heightAnchor.constraint(equalToConstant: 40),
+            
+            storyCollectionView.topAnchor.constraint(equalTo: navBarView.bottomAnchor),
+            storyCollectionView.leadingAnchor.constraint(equalTo: view.leadingAnchor),
+            storyCollectionView.trailingAnchor.constraint(equalTo: vцiew.trailingAnchor),
+            storyCollectionView.heightAnchor.constraint(equalToConstant: 110)
         ])
-        view.backgroundColor = .systemBackground
-        self.navigationController?.setNavigationBarHidden(true, animated: false)
-        
-        registerForTraitChanges([UITraitUserInterfaceStyle.self], handler: { (self: Self, previousTraitCollection: UITraitCollection) in
-            if self.traitCollection.userInterfaceStyle == .light {
-                lightMode()
-                
-            } else {
-                darkMode()
-            }
-        })
-        
-       func lightMode(){
-            self.navBarView.buttonAddPost.tintColor = .black
-            self.navBarView.buttonLike.tintColor = .black
-            self.navBarView.buttonMessage.tintColor = .black
-            self.navBarView.label.textColor = .black
+    }
+    
+    func lightMode(){
+        self.navBarView.buttonAddPost.tintColor = .black
+        self.navBarView.buttonLike.tintColor = .black
+        self.navBarView.buttonMessage.tintColor = .black
+        self.navBarView.label.textColor = .black
+    }
+    
+    func darkMode(){
+        self.navBarView.buttonAddPost.tintColor = .white
+        self.navBarView.buttonLike.tintColor = .white
+        self.navBarView.buttonMessage.tintColor = .white
+        self.navBarView.label.textColor = .white
+    }
+    
+    func getCompositionalLayout() -> UICollectionViewCompositionalLayout {
+        return UICollectionViewCompositionalLayout { (section, environment) ->
+            NSCollectionLayoutSection? in
+            let itemSize = NSCollectionLayoutSize(widthDimension: .estimated(1), heightDimension: .fractionalHeight(1))
+            let item = NSCollectionLayoutItem(layoutSize: itemSize)
+            let groupSize = NSCollectionLayoutSize(widthDimension: .estimated(1), heightDimension: .fractionalHeight(1))
+            let group = NSCollectionLayoutGroup.horizontal(layoutSize: groupSize, subitems: [item])
+            let layoutSection = NSCollectionLayoutSection(group: group)
+            layoutSection.interGroupSpacing = 5
+            layoutSection.orthogonalScrollingBehavior = .continuous
+            return layoutSection
         }
-        func darkMode(){
-            self.navBarView.buttonAddPost.tintColor = .white
-            self.navBarView.buttonLike.tintColor = .white
-            self.navBarView.buttonMessage.tintColor = .white
-            self.navBarView.label.textColor = .white
+    }
+    
+    func storyCollectionViewParameters(){
+        storyCollectionView.delegate = self
+        storyCollectionView.register(StoryCollectionViewCell.self, forCellWithReuseIdentifier: StoryCollectionViewCell.id)
+    }
+    
+    func navControllerParameters(){
+        self.navigationController?.setNavigationBarHidden(true, animated: false)
+    }
+    func fillTitleItems(){
+        for i in 1...10{
+            titleItems.append(StoryCellItem(title: "avatar\(i)"))
         }
     }
 }
-
