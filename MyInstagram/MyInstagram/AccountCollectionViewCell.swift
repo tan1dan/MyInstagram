01c9e10a5246022421a53d1b5ed9723665f220df
Ivan Znak <ivanznak04@gmail.com>
2024-01-21 12:17:59 +0100

    AccountCollectionViewCell

--- MyInstagram/MyInstagram/AccountCollectionViewCell.swift
+++ MyInstagram/MyInstagram/AccountCollectionViewCell.swift
@@ -5,4 +5,27 @@
 //  Created by Иван Знак on 13/01/2024.
 //
 
-import Foundation
+import UIKit
+
+class AccountCollectionViewCell: UICollectionViewCell {
+    static let id = "Account"
+    let imageView = UIImageView()
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        addSubview(imageView)
+        imageView.translatesAutoresizingMaskIntoConstraints = false
+        NSLayoutConstraint.activate([
+            imageView.topAnchor.constraint(equalTo: topAnchor),
+            imageView.leadingAnchor.constraint(equalTo: leadingAnchor),
+            imageView.trailingAnchor.constraint(equalTo: trailingAnchor),
+            imageView.bottomAnchor.constraint(equalTo: bottomAnchor)
+        ])
+        imageView.contentMode = .scaleAspectFill
+        imageView.clipsToBounds = true
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+}
